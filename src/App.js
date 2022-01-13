fcb48edbbb3b262d76c280cf26c52856a0babdd0
Asjag <asmitajagtap1111@gmail.com>
2022-01-13 13:30:56 +0530

    my second project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Button} from 'react-bootstrap'
+function App() {
+  return (
+    <div >
+      <h1>Ecommerce dashbord</h1>
+      <button>normal button</button>
+      <Button>bootstrap butoon</Button>
+    </div>
+  );
+}
+
+export default App;
